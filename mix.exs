2a986410c1d7ca4675f69ba3584b2f669d416f37
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2025-05-17 15:48:50 +0300

    Add description in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule PlugLoopback.MixProject do
   def project do
     [
       app: :plug_loopback,
+      description: "A set of Plug utils to programmatically call your own endpoints",
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
